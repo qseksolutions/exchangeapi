572287f9a94baa5c927c1c3c28c9499005ec9cfb
ASHOK\Ashok Desai <ashok.desai64@gmail.com>
2018-03-20 11:46:12 +0530

    ashok

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,8 +1,8 @@
-// export const base_url = 'https://exchangeapi.xyz/';
-export const base_url = 'http://localhost:4200/';
+export const base_url = 'https://exchangeapi.xyz/';
+//export const base_url = 'http://localhost:4200/';
 
-// export const api_url = 'https://api.exchangeapi.xyz/app/api/v1/';
-export const api_url = 'http://52.52.127.230:3000/api/v1/';
+export const api_url = 'https://api.exchangeapi.xyz/app/api/v1/';
+//export const api_url = 'http://52.52.127.230:3000/api/v1/';
 export const exchangelist = 'exchange';
 export const allexchangelist = 'allexchange';
 export const singlecoinlist = 'singlecoin';
@@ -11,8 +11,8 @@ export const getbidaskandorderlist = 'getbidaskandorder';
 export const getcoinlistlist = 'getcoinlist';
 export const getexchangelistlist = 'getexchangelist';
 
-// export const new_api_url = 'https://influx.exchangeapi.xyz/app/influxdb/';
-export const new_api_url = 'http://35.174.58.234:3000/trade/';
+export const new_api_url = 'https://influx.exchangeapi.xyz/app/trade/';
+//export const new_api_url = 'http://35.174.58.234:3000/trade/';
 export const alltradelist = 'alltrade';
 export const tradebyexchangelist = 'tradebyexchange';
 
